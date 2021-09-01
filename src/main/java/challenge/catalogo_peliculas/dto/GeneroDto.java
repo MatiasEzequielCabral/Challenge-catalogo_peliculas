74fc36583f234520f1607b128a2aceac9fef057f
matias <cabralmatiasezequiel@gmail.com>
2021-09-01 18:22:02 -0300

    feat: se agrega el patron Dto para la clase Genero

---
+++ src/main/java/challenge/catalogo_peliculas/dto/GeneroDto.java
@@ -0,0 +1,34 @@
+package challenge.catalogo_peliculas.dto;
+
+public class GeneroDto {
+    private String nombre;
+    private String imagen;
+    private Long idPelicula;
+
+    public GeneroDto() {
+    }
+
+    public Long getIdPelicula() {
+        return idPelicula;
+    }
+
+    public void setIdPelicula(Long idPelicula) {
+        this.idPelicula = idPelicula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+}
