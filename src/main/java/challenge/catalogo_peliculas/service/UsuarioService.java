d52146bfb69d688ac435b6952ae13026f213991a
matias <cabralmatiasezequiel@gmail.com>
2021-09-01 18:19:12 -0300

    chore: se elimina la clase usuario, su controller, el repository y service

--- src/main/java/challenge/catalogo_peliculas/service/UsuarioService.java
+++
@@ -1,9 +0,0 @@
-package challenge.catalogo_peliculas.service;
-
-import org.springframework.ui.Model;
-
-public interface UsuarioService {
-
-    String viewHomePage();
-    String formularioRegistro(Model model);
-}
