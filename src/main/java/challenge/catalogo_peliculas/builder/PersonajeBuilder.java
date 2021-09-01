3a30b4901857f85d31bc1ab83b56c5c763527090
matias <cabralmatiasezequiel@gmail.com>
2021-09-01 18:21:15 -0300

    feat: se agrega el builder para la clase Personaje

---
+++ src/main/java/challenge/catalogo_peliculas/builder/PersonajeBuilder.java
@@ -0,0 +1,28 @@
+package challenge.catalogo_peliculas.builder;
+
+import challenge.catalogo_peliculas.data.Pelicula;
+import challenge.catalogo_peliculas.data.Personaje;
+import challenge.catalogo_peliculas.dto.PeliculaDto;
+import challenge.catalogo_peliculas.dto.PersonajeDto;
+
+public class PersonajeBuilder {
+
+    private String imagen;
+    private String nombre;
+    private int edad;
+    private int peso;
+    private String historia;
+
+    public PersonajeBuilder withPersonajeDto(PersonajeDto personajeDto){
+        this.imagen = personajeDto.getImagen();
+        this.nombre = personajeDto.getNombre();
+        this.edad = personajeDto.getEdad();
+        this.peso = personajeDto.getPeso();
+        this.historia = personajeDto.getHistoria();
+        return this;
+    }
+
+    public Personaje build(){
+        return new Personaje(this.imagen, this.nombre, this.edad, this.peso, this.historia) ;
+    }
+}
