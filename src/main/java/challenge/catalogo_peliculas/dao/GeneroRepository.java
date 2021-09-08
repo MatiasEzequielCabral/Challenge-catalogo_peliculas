3f477ba0a41167d0917594b9dfb678d88ad83419
matias <cabralmatiasezequiel@gmail.com>
2021-09-08 17:13:48 -0300

    refactor: se elimino una funcion para mostrar los generos por id en genero repesoritory

--- src/main/java/challenge/catalogo_peliculas/dao/GeneroRepository.java
+++ src/main/java/challenge/catalogo_peliculas/dao/GeneroRepository.java
@@ -1,7 +1,9 @@
 package challenge.catalogo_peliculas.dao;
 
 import challenge.catalogo_peliculas.data.Genero;
+import challenge.catalogo_peliculas.data.Pelicula;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +11,4 @@ import java.util.List;
 @Repository
 public interface GeneroRepository extends JpaRepository<Genero, Long> {
 
-    //@Query("select genero.peliculas from Genero genero")
-    List<Genero> getAllPeliculasById(Long id);
 }
