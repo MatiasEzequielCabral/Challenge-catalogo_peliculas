8ddbb7c25ffd5f8ffbce53ecc8e88bda33c1f21f
matias <cabralmatiasezequiel@gmail.com>
2021-09-09 13:27:14 -0300

    feat: se agrego la clase PeliculaCrearDto

---
+++ src/main/java/challenge/catalogo_peliculas/dto/PeliculaCrearDto.java
@@ -0,0 +1,35 @@
+package challenge.catalogo_peliculas.dto;
+
+public class PeliculaCrearDto {
+
+    private String imagen;
+    private String titulo;
+    private int calificacion;
+
+    public PeliculaCrearDto() {
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(int calificacion) {
+        this.calificacion = calificacion;
+    }
+}
