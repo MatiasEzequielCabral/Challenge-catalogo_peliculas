55cb9c39ca61598d844f1553b5ff75740441e2a1
matias <cabralmatiasezequiel@gmail.com>
2021-09-09 13:27:24 -0300

    feat: se agrego la clase PersonajeCrearDto

---
+++ src/main/java/challenge/catalogo_peliculas/dto/PersonajeCrearDto.java
@@ -0,0 +1,53 @@
+package challenge.catalogo_peliculas.dto;
+
+public class PersonajeCrearDto {
+
+    private String imagen;
+    private String nombre;
+    private int edad;
+    private int peso;
+    private String historia;
+
+    public PersonajeCrearDto() {
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public String getHistoria() {
+        return historia;
+    }
+
+    public void setHistoria(String historia) {
+        this.historia = historia;
+    }
+}
