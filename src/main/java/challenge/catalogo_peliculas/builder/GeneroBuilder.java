e9a90c4544f1d88aab524704bb2dc2cd95afa1e5
matias <cabralmatiasezequiel@gmail.com>
2021-09-09 13:28:55 -0300

    feat: se agrego la funcion withGeneroDto con un nuevo tipo de variable

--- src/main/java/challenge/catalogo_peliculas/builder/GeneroBuilder.java
+++ src/main/java/challenge/catalogo_peliculas/builder/GeneroBuilder.java
@@ -1,14 +1,21 @@
 package challenge.catalogo_peliculas.builder;
 
 import challenge.catalogo_peliculas.data.Genero;
-import challenge.catalogo_peliculas.dto.GeneroDto;
+import challenge.catalogo_peliculas.dto.GeneroCrearDto;
+import challenge.catalogo_peliculas.dto.GeneroEditarDto;
 
 public class GeneroBuilder {
 
     private String nombre;
     private String imagen;
 
-    public GeneroBuilder withGeneroDto(GeneroDto generoDto){
+    public GeneroBuilder withGeneroDto(GeneroCrearDto generoDto){
+        this.nombre = generoDto.getNombre();
+        this.imagen = generoDto.getImagen();
+        return this;
+    }
+
+    public GeneroBuilder withGeneroDto(GeneroEditarDto generoDto){
         this.nombre = generoDto.getNombre();
         this.imagen = generoDto.getImagen();
         return this;
