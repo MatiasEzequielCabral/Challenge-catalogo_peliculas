219668e0afca96e1b1a1576e45133973a0c4faef
matias <cabralmatiasezequiel@gmail.com>
2021-09-11 19:36:32 -0300

    refactor: se reemplazo la case user de userbuilder

--- src/main/java/challenge/catalogo_peliculas/service/UserDetailsServiceImpl.java
+++ src/main/java/challenge/catalogo_peliculas/service/UserDetailsServiceImpl.java
@@ -1,14 +1,16 @@
 package challenge.catalogo_peliculas.service;
 
-import challenge.catalogo_peliculas.builder.UsuarioBuilder;
 import challenge.catalogo_peliculas.dao.UsuarioRepository;
 import challenge.catalogo_peliculas.data.Usuario;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService{
@@ -17,18 +19,17 @@ public class UserDetailsServiceImpl implements UserDetailsService{
     private UsuarioRepository usuarioRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
-
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Usuario usuario = usuarioRepository.findByUsername(username);
-        User.UserBuilder userBuilder = null;
-        if(usuario != null) {
+        UserBuilder userBuilder = null;
+        if (usuario != null) {
             userBuilder = User.withUsername(username);
             userBuilder.disabled(false);
             userBuilder.password(usuario.getPassword());
             userBuilder.authorities(new SimpleGrantedAuthority("ROL_USER"));
-            } else {
-                throw new UsernameNotFoundException("Usuario no registrado");
-            }
+        } else {
+            throw new UsernameNotFoundException("Usuario no registrado");
+        }
         return userBuilder.build();
     }
 }
