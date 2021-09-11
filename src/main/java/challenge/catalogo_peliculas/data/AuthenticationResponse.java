f1ae32236fe3afc6a8238b53c498a7854f9ec5c4
matias <cabralmatiasezequiel@gmail.com>
2021-09-11 19:25:58 -0300

    feat: se agrego la authetication response

---
+++ src/main/java/challenge/catalogo_peliculas/data/AuthenticationResponse.java
@@ -0,0 +1,18 @@
+package challenge.catalogo_peliculas.data;
+
+public class AuthenticationResponse {
+
+    private String token;
+
+    public AuthenticationResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
