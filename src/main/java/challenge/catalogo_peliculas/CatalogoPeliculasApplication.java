7fc9ce677565c8528496b295f8f0bc64059668f6
matias <cabralmatiasezequiel@gmail.com>
2021-09-13 17:06:17 -0300

    refactor: se habilito la libreria de swagger

--- src/main/java/challenge/catalogo_peliculas/CatalogoPeliculasApplication.java
+++ src/main/java/challenge/catalogo_peliculas/CatalogoPeliculasApplication.java
@@ -2,8 +2,10 @@ package challenge.catalogo_peliculas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CatalogoPeliculasApplication {
 
     public static void main(String[] args) {
