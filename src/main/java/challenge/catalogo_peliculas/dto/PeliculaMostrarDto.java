e202730046fd82eaeb2965812c7deea5fca4331e
matias <cabralmatiasezequiel@gmail.com>
2021-09-12 21:35:13 -0300

    feat: se creo el dto para mostrar solo peliculas

---
+++ src/main/java/challenge/catalogo_peliculas/dto/PeliculaMostrarDto.java
@@ -0,0 +1,22 @@
+package challenge.catalogo_peliculas.dto;
+
+import challenge.catalogo_peliculas.data.Personaje;
+
+import java.util.List;
+
+public class PeliculaMostrarDto {
+
+    private List<Personaje> personajes;
+
+    public PeliculaMostrarDto(List<Personaje> personajes) {
+        this.personajes = personajes;
+    }
+
+    public List<Personaje> getPersonajes() {
+        return personajes;
+    }
+
+    public void setPersonajes(List<Personaje> personajes) {
+        this.personajes = personajes;
+    }
+}
