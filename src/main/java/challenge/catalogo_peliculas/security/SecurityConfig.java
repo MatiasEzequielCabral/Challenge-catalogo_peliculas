2539a3b5b0e6b42502f1e6cbb97348b8e8038ae0
matias <cabralmatiasezequiel@gmail.com>
2021-09-14 13:43:56 -0300

    refactor: se creo una lista de endpoints habilitados para ingresar

--- src/main/java/challenge/catalogo_peliculas/security/SecurityConfig.java
+++ src/main/java/challenge/catalogo_peliculas/security/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.csrf().disable().authorizeRequests()
-                .antMatchers("AUTH_WHITELIST").permitAll()
+                .antMatchers(AUTH_WHITELIST).permitAll()
                 .anyRequest().authenticated()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
