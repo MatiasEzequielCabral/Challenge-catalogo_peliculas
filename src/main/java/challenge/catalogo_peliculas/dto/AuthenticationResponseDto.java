8e5369059047cc1b388085cdea67c205626aec42
matias <cabralmatiasezequiel@gmail.com>
2021-09-26 23:30:11 -0300

    refactor: se agrego la notacion all args constru.

--- src/main/java/challenge/catalogo_peliculas/dto/AuthenticationResponseDto.java
+++ src/main/java/challenge/catalogo_peliculas/dto/AuthenticationResponseDto.java
@@ -1,10 +1,12 @@
 package challenge.catalogo_peliculas.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class AuthenticationResponseDto {
 
     private String token;
